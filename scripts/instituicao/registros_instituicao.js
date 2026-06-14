970e2245166a8eb754e017dba02ca667b6511eb1
lecs2 <cavalcantesantosluiseduardo@gmail.com>
2026-06-14 13:40:59 -0300

    adiciona documentos e povoamento das coleções

---
+++ scripts/instituicao/registros_instituicao.js
@@ -0,0 +1,17 @@
+export const instituicoes = [
+  { "id": 1, "nome": "Embrapa Recursos Genéticos e Biotecnologia", "país": "Brasil", "contato": { "e-mails": ["banco@embrapa-rg.br"], "telefones": [556134484000] } },
+  { "id": 2, "nome": "Millennium Seed Bank - Kew Gardens", "país": "Reino Unido", "contato": { "e-mails": ["seedbank@kew.org"], "telefones": [442083325000] } },
+  { "id": 3, "nome": "INTA - Instituto Nacional de Tecnología Agropecuaria", "país": "Argentina", "contato": { "e-mails": ["bancosemillas@inta.gob.ar"], "telefones": [541143063000] } },
+  { "id": 4, "nome": "USDA National Plant Germplasm System", "país": "Estados Unidos", "contato": { "e-mails": ["npgs@usda.gov"], "telefones": [13012345678] } },
+  { "id": 5, "nome": "CIRAD Banque de Gènes", "país": "França", "contato": { "e-mails": ["genebank@cirad.fr"], "telefones": [33467615800] } },
+  { "id": 6, "nome": "Svalbard Global Seed Vault", "país": "Noruega", "contato": { "e-mails": ["seedvault@nordgen.org"], "telefones": [4779023000] } },
+  { "id": 7, "nome": "Vavilov Institute of Plant Industry", "país": "Rússia", "contato": { "e-mails": ["info@vir.nw.ru"], "telefones": [78123144848] } },
+  { "id": 8, "nome": "ICAR - National Bureau of Plant Genetic Resources", "país": "Índia", "contato": { "e-mails": ["nbpgr.director@icar.gov.in"], "telefones": [911125843697] } },
+  { "id": 9, "nome": "AVRDC - The World Vegetable Center", "país": "Taiwan", "contato": { "e-mails": ["info@worldveg.org"], "telefones": [88665837801] } },
+  { "id": 10, "nome": "CIMMYT - Int. Maize and Wheat Improvement Center", "país": "México", "contato": { "e-mails": ["cimmyt@cgiar.org"], "telefones": [525558042004] } },
+  { "id": 11, "nome": "IRRI - International Rice Research Institute", "país": "Filipinas", "contato": { "e-mails": ["info@irri.org"], "telefones": [63285805600] } },
+  { "id": 12, "nome": "CIAT - Int. Center for Tropical Agriculture", "país": "Colômbia", "contato": { "e-mails": ["ciat@cgiar.org"], "telefones": [5724450000] } },
+  { "id": 13, "nome": "CIP - International Potato Center", "país": "Peru", "contato": { "e-mails": ["cip@cgiar.org"], "telefones": [5113496017] } },
+  { "id": 14, "nome": "ICARDA - Int. Center for Agricultural Research", "país": "Líbano", "contato": { "e-mails": ["icarda@cgiar.org"], "telefones": [9611813303] } },
+  { "id": 15, "nome": "ICRISAT - Int. Crops Research Institute", "país": "Índia", "contato": { "e-mails": ["icrisat@cgiar.org"], "telefones": [914030713071] } }
+];
---
+++ scripts/sala/registros_sala.js
@@ -0,0 +1,17 @@
+export const salas = [
+  { "id": "A001", "capacidade": 5000 },
+  { "id": "A002", "capacidade": 3000 },
+  { "id": "B001", "capacidade": 8000 },
+  { "id": "B002", "capacidade": 4500 },
+  { "id": "C001", "capacidade": 10000 },
+  { "id": "C002", "capacidade": 6000 },
+  { "id": "D001", "capacidade": 7500 },
+  { "id": "D002", "capacidade": 3500 },
+  { "id": "E001", "capacidade": 12000 },
+  { "id": "E002", "capacidade": 5500 },
+  { "id": "F001", "capacidade": 9000 },
+  { "id": "F002", "capacidade": 4000 },
+  { "id": "G001", "capacidade": 15000 },
+  { "id": "G002", "capacidade": 6500 },
+  { "id": "H001", "capacidade": 20000 }
+];
